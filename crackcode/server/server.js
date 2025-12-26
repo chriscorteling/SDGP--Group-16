e0d119e3cd6494cfeb86e312b5e14d28e141328a
Sasni Lasadi <sasnilasadi410@gmail.com>
2025-12-26 17:34:37 +0530

    Connected mongodb and redis to backend

--- crackcode/server/server.js
+++ crackcode/server/server.js
@@ -9,12 +9,17 @@ app.use(express.json());
 app.use(cors());
 
 // 1. Connect to MongoDB (Source of Truth)
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 5050 // 5 seconds timeout
+})
     .then(() => console.log("✅ MongoDB Connected"))
     .catch(err => console.error("❌ MongoDB Connection Error:", err));
 
 // 2. Connect to Redis (Real-time Leaderboard)
 const redis = new Redis(process.env.REDIS_URL);
+redis.on('error', (err) => {
+    console.log("❌ Redis is not responding. Is the server running?");
+});
 redis.on('connect', () => console.log("✅ Redis Connected"));
 redis.on('error', (err) => console.error("❌ Redis Error:", err));
 
@@ -22,3 +27,4 @@ app.get('/', (req, res) => res.send("CrackCode Backend API is Running!"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
